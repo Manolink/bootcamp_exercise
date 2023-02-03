3d49bc649bb1f8721f9b19ff8ba193d19d352f14
Manolink <109784081+Manolink@users.noreply.github.com>
2023-02-03 15:33:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Escribe tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Tu nombre es:   {nombre}");
